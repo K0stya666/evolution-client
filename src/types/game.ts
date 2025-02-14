b1b376a39691814ac3682a4a8706681d76806df8
Kostya666 <eliseevkonstantin306@gmail.com>
2025-02-14 21:46:14 +0300

    add types

---
+++ src/types/game.ts
@@ -0,0 +1,36 @@
+export type Phase =
+    | "growth"             // фаза развития
+    | "foodCalculation"    // фаза определения кормовой базы
+    | "feeding"            // фаза питания
+    | "extinction";        // фаза вымирания
+
+export interface Card {
+    id: string;
+    title: string;
+    description: string;
+    // Признак, что карта может давать дополнительную потребность в еде, например
+    extraFoodNeeded?: number;
+}
+
+export interface Player {
+    id: string;
+    username: string;
+    hand: Card[];
+    animals: Animal[];
+}
+
+export interface Animal {
+    id: string;
+    cards: Card[];  // свойства (карты), наложенные на животное
+    foodNeeded: number; // сколько фишек нужно, чтобы накормить
+    foodEaten: number;  // сколько фишек уже получено
+}
+
+export interface GameState {
+    gameId: string;
+    players: Player[];
+    currentPlayerId: string;
+    phase: Phase;
+    foodBase: number;    // текущее количество фишек в кормовой базе
+    round: number;
+}
