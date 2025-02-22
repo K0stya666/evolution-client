b14987aeb3fbfd403af9d88a9b7c021f8b440f62
Kostya666 <eliseevkonstantin306@gmail.com>
2025-02-22 20:10:19 +0300

    add card template

---
+++ src/components/deck/Card.tsx
@@ -0,0 +1,16 @@
+// src/components/Card/Card.tsx
+
+import React from "react";
+import "./Card.css";
+
+const Card: React.FC = () => {
+
+
+    return (
+        <div className="card">
+            <p>hui</p>
+        </div>
+    );
+};
+
+export default Card;
