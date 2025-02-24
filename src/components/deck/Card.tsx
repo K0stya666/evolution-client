e2963991f16e459584c2f204cd69cedb59e44351
Kostya666 <eliseevkonstantin306@gmail.com>
2025-02-24 09:25:09 +0300

    add condition on card

--- src/components/deck/Card.tsx
+++ src/components/deck/Card.tsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "./card.css";
 
-export interface Card {
-    title: string;
-    description: string;
-    extraFoodNeeded?: number;
+interface Condition {
+    condition: React.ReactNode;
 }
 
-const Card: React.FC = () => {
+const Card: React.FC<Condition> = ({ condition }) => {
+    const [flipped, setFlipped] = useState(false);
 
+    const handleClick = () => {
+        setFlipped(!flipped);
+    }
 
     return (
-        <div className="card-template">
-            <p>hui</p>
+        <div className="flip-card" onClick={handleClick}>
+            <div className={`flip-card-inner ${flipped ? "flipped" : ""}`}>
+                <div className="flip-card-front">
+                    {/*<CardFront/>*/}
+                    <div className="card-template">
+                        {/*<p>hui</p>*/}
+                        {condition && <div className="condition-overlay">{condition}</div>}
+                    </div>
+
+                </div>
+                <div className="flip-card-back">
+                    {/*<CardBack/>*/}
+                    <div className="card-template">
+                        <img src="/images/lizard.png" alt="Логотип карты" className="lizard"/>
+                    </div>
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
 
 export default Card;
---
+++ src/components/deck/conditions/Swimming.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Swimming: React.FC = () => {
+
+
+
+
+    return (
+        <div className="swimming">
+            <p>ВОДОПЛАВАЮЩЕЕ</p>
+        </div>
+    )
+}
+export default Swimming;
